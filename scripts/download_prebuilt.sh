8acccc4847aaf8dc3ea3917c7088d556026edcaf
Yilong Wang <2548340423@qq.com>
2024-04-17 22:47:19 +0800

    update script

--- scripts/download_prebuilt.sh
+++ scripts/download_prebuilt.sh
@@ -3,6 +3,7 @@
 mkdir -p polyos && cd polyos
 export OH_HOME=`pwd`
 export OH_IMAGES=$OH_HOME/images
-wget https://filebin.net/m6q51s538bgd12p1/images.tar.gz
-tar -zxvf images.tar.gz
-
+if [ ! -d "images" ]; then
+    wget https://filebin.net/m6q51s538bgd12p1/images.tar.gz
+    tar -zxvf images.tar.gz
+fi
