9c569cc59f9b051e1c97510f7927685f111e2f3e
Yilong Wang <2548340423@qq.com>
2024-04-22 18:14:37 +0800

    edit script

--- scripts/download_prebuilt.sh
+++ scripts/download_prebuilt.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 mkdir -p polyos && cd polyos
-export OH_HOME=`pwd`
-export OH_IMAGES=$OH_HOME/images
 if [ ! -d "images" ]; then
-    wget https://filebin.net/m6q51s538bgd12p1/images.tar.gz
-    tar -zxvf images.tar.gz
-    mkdir -p out/riscv64_virt/packages/phone/images
+    wget https://filebin.net/m6q51s538bgd12p1/images.tar.gz &&
+    tar -zxvf images.tar.gz &&
+    mkdir -p out/riscv64_virt/packages/phone/images &&
     mv images/* out/riscv64_virt/packages/phone/images
 fi
